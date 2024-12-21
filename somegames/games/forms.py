a9190124d98d88df22ce23bd58837503448238df
Antonymous <antos.misztal@gmail.com>
2024-12-21 23:04:13 +0100

    Dodano formularz poprzez forms.py

---
+++ somegames/games/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+class LoginForm(forms.Form):
+
+    username = forms.CharField(
+        max_length=36,
+        label='Nazwa',
+        widget=forms.TextInput,
+    )
+
+    password = forms.CharField(
+        min_length=8,
+        label='Hasło',
+        widget=forms.PasswordInput,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-input',
+            'id': 'usernameInput',
+            'name': 'username',
+            'placeholder': 'Nazwa tutaj....',
+        })
+
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-input',
+            'id': 'passwordInput',
+            'name': 'password',
+            'placeholder': 'Hasło tutaj....',
+        })
